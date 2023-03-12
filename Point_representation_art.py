68c0b65070eff5d403149400ba54e7bd78b4eaa9
leeeju <02stu4@gmail.com>
2023-03-12 20:56:36 +0900

    업데이트

--- Point_representation_art.py
+++ Point_representation_art.py
@@ -6,16 +6,19 @@ from PyQt5.QtCore import Qt
 class MyCanvas(QWidget):
     def __init__(self):
         super().__init__()
-        self.setGeometry(100, 100, 500, 500) # 위치와 크기 설정
-        self.setWindowTitle('My_Canvas')
-        self.show()
+        self.setGeometry(200, 200, 400, 400) # 위치와 크기 설정/ 파리미터 변경
+        self.setWindowTitle('My_Canvas') #  gui이름 정하기
+        self.show() # 출력
 
     def paintEvent(self, event):
         painter = QPainter()
         painter.begin(self)
-        pen = QPen(QColor(0, 0, 0), 5, Qt.SolidLine)
-        painter.setPen(pen)
-        painter.drawPoint(100, 100) # 점 그리기
+        pen = QPen(QColor(150, 150, 150), 10, Qt.SolidLine) # HSV Color Format ( 0~255 ) , size(10)
+        painter.setPen(pen) # 색 지점 출력
+        painter.drawPoint(100, 100) , painter.drawPoint(500, 300) , painter.drawPoint(500, 300) # 점 그리기
+        painter.drawPoint(200, 100) , painter.drawPoint(300, 200) , painter.drawPoint(500, 300)
+        painter.drawPoint(400, 500) , painter.drawPoint(500, 200) , painter.drawPoint(500, 300)
+        painter.drawPoint(500, 300) , painter.drawPoint(500, 300) , painter.drawPoint(500, 300)
         painter.end()
 
 if __name__ == '__main__':
